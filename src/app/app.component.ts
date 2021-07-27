7865e4edeb08c7a3b677f23bc60d06b52d2de746
OmidErnesto <ochahuaris@unsa.edu.pe>
2021-07-27 15:18:24 -0500

    Modificacion del index y estilo y ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name = "Omid Chahuaris Ch.";
+  email = "ochahuaris@unsa.edu.pe";
+  webpage = "http://www.unsa.edu.pe";
 }
